0a7df506345ccc16fda4ad70c0c81483579343dd
Wojciech Nojszewski <92394209+nojszewski@users.noreply.github.com>
2023-10-28 18:42:36 +0200

    Initial commit

---
+++ app/config.php
@@ -0,0 +1,20 @@
+<?php
+
+    /* MySQL Database credentials */
+
+        // MySQL DB Name
+        public const DB_NAME = 'pmsp1';
+
+        // MySQL DB Host
+        public const DB_HOST = 'localhost';
+
+        // MySQL DB Username
+        public const DB_USER = 'root';
+
+        // MySQL DB Password
+        public const DB_PASSWORD = '';
+
+
+    /* End of MySQL Database credentials */
+
+?>
